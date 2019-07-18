86a573ed68569b6b2e61ba1448977366ec99733c
KaMeHb-UA <marlock@etlgr.com>
2019-07-18 17:09:41 +0300

    Update prefix.mjs

--- src/prefix.mjs
+++ src/prefix.mjs
@@ -1,1 +1,1 @@
-const s={lightGallery:null,lgData:null,lgModules:null},window=new Proxy(window,{get:(t,p)=>(p in s?s:t)[p],set:(t,p,v)=>((p in s?s:t)[p]=v,true)});
+const s={lightGallery:null,lgData:null,lgModules:null},_window=window,window=new Proxy(_window,{get:(t,p)=>(p in s?s:t)[p],set:(t,p,v)=>((p in s?s:t)[p]=v,true)});

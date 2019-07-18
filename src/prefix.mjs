38146a35959bee5a1b80b7708d28fc133de03a51
KaMeHb-UA <marlock@etlgr.com>
2019-07-18 17:06:40 +0300

    export fixed (maybe)

--- src/prefix.mjs
+++ src/prefix.mjs
@@ -1,1 +1,1 @@
-const module={exports:{}},exports=module.exports;
+const s={lightGallery:null,lgData:null,lgModules:null},window=new Proxy(window,{get:(t,p)=>(p in s?s:t)[p],set:(t,p,v)=>((p in s?s:t)[p]=v,true)});
